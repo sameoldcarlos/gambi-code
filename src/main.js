82392161b0da090114ce5b5056d105a6792a6c3e
sameoldcarlos <albertocarlos221@gmail.com>
2023-01-02 04:42:55 +0000

    chore: add vue feather icons library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueFeather from 'vue-feather';
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+app.component(VueFeather.name, VueFeather);
 
 app.mount('#app')
